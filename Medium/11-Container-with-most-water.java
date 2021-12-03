3ad446acd269368720daf9a0fb9fe38b557ae767
Adamo Orsini <adamo.orsini12@gmail.com>
2021-12-02 22:15:16 -0500

    December 2nd

---
+++ Medium/11-Container-with-most-water.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxArea(int[] height) {
+        
+        // Initialize left, right and max area
+        int leftEnd = 0, rightEnd = height.length - 1;
+        int maxArea = 0;
+        
+        while (leftEnd < rightEnd) {
+            // Find max of water area (width = right - left, height = lowest of left and right heights)
+            maxArea = Math.max(maxArea, (rightEnd - leftEnd) * Math.min(height[leftEnd], height[rightEnd]));
+            
+            // Since we start with the largest width (both extremes), only increment smaller height
+            if (height[rightEnd] > height[leftEnd])
+                leftEnd++;
+            else
+                rightEnd--;
+        }
+        
+        return maxArea;
+    }
+}
